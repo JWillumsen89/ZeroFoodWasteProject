fd4cd7ff6137a5ff6ad98ab99f333b65c3eca0f4
JWillumsen89 <100369030+JWillumsen89@users.noreply.github.com>
2022-09-19 14:21:46 +0200

    Initial commit

---
+++ src/main/java/dk/jwillum/websitedemo/controller/MainController.java
@@ -0,0 +1,59 @@
+package dk.jwillum.websitedemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+
+  @GetMapping("/index")
+  public String index() {
+    return "index";
+  }
+
+  @GetMapping("/indexcontent")
+  public String content() {
+    return "indexcontent";
+  }
+
+  @GetMapping("/news")
+  public String newsSite() {
+    return "news";
+  }
+
+  @GetMapping("/about")
+  public String aboutSite() {
+    return "about";
+  }
+
+  @GetMapping("/contact")
+  public String contactSite() {
+    return "contact";
+  }
+
+  @GetMapping("/shop")
+  public String shopSite() {
+    return "shop";
+  }
+
+  @GetMapping("/gallery")
+  public String gallerySite() {
+    return "gallery";
+  }
+
+  @GetMapping("/events")
+  public String eventsSite() {
+    return "events";
+  }
+
+  @GetMapping("/dropdownmenu")
+  public String dropDownMenuSite() {
+    return "dropdownmenu";
+  }
+
+  @GetMapping("/homebutton")
+  public String homeButtonSite() {
+    return "homebutton";
+  }
+}
---
+++ src/test/java/dk/jwillum/websitedemo/WebsiteDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.jwillum.websitedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsiteDemoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
